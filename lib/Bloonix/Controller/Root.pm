a197fdaef2f1f388ff4c212ec2a07d440f26f735
Jonny Schulz <support@bloonix.de>
2014-11-25 16:14:56 +0100

    New Build

--- lib/Bloonix/Controller/Root.pm
+++ lib/Bloonix/Controller/Root.pm
@@ -7,7 +7,7 @@ use Time::HiRes;
 sub auto {
     my ($self, $c) = @_;
 
-    $c->version->{js} = 71;
+    $c->version->{js} = 72;
 
     my $addr = $c->req->remote_addr || "n/a";
     my $lang = $c->req->cookie("lang");
