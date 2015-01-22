152ae813f9786503312dbf3e2a16d892455474cf
Jonny Schulz <support@bloonix.de>
2015-01-22 16:55:40 +0100

    js version increased

--- lib/Bloonix/Controller/Root.pm
+++ lib/Bloonix/Controller/Root.pm
@@ -7,7 +7,7 @@ use Time::HiRes;
 sub auto {
     my ($self, $c) = @_;
 
-    $c->version->{js} = 81;
+    $c->version->{js} = 82;
 
     my $addr = $c->req->remote_addr || "n/a";
     my $lang = $c->req->cookie("lang");
