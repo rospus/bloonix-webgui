38daf6124e8e55a2a3ee027d0f641aebeb180d85
Jonny Schulz <support@bloonix.de>
2015-08-23 22:50:40 +0200

    Increased json version

--- lib/Bloonix/Controller/Root.pm
+++ lib/Bloonix/Controller/Root.pm
@@ -7,7 +7,7 @@ use Time::HiRes;
 sub auto {
     my ($self, $c) = @_;
 
-    $c->version->{js} = 101;
+    $c->version->{js} = 102;
 
     my $addr = $c->req->remote_addr || "n/a";
     my $lang = $c->req->cookie("lang");
